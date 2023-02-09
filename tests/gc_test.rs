1eb93acc8a568fbef2f8c13a7a0a07f3514ca5ca
lumiknit <aasr4r4@gmail.com>
2023-02-09 18:02:22 +0900

    Fix tests

--- tests/gc_test.rs
+++ tests/gc_test.rs
@@ -3,15 +3,13 @@ use ssm::gc::*;
 
 #[test]
 fn create_mem() {
-    let _mem = Mem::new(128, 128, 1024, 2048);
+    let _mem = Mem::new(128, 128, 1024);
 }
 
 #[test]
 fn allocate_short() {
-    let mut mem = Mem::new(128, 128, 1024, 2048);
+    let mut mem = Mem::new(128, 128, 1024);
     let tup = mem.alloc_short(4, 12);
-    assert!(tup.is_ok());
-    let tup = tup.unwrap();
     let hd = tup.header();
     assert!(!hd.is_long());
     assert!(hd.is_white());
@@ -20,9 +18,9 @@ fn allocate_short() {
 
 #[test]
 fn allocate_shorts() {
-    let mut mem = Mem::new(128, 128, 1024, 2048);
-    let tup1 = mem.alloc_short(4, 1).unwrap();
-    let tup2 = mem.alloc_short(4, 2).unwrap();
+    let mut mem = Mem::new(128, 128, 1024);
+    let tup1 = mem.alloc_short(4, 1);
+    let tup2 = mem.alloc_short(4, 2);
     for i in 0..4 {
         tup1.set_val(i, Val(i + 1));
         tup2.set_val(i, Val(i + 42));
@@ -38,92 +36,88 @@ fn allocate_shorts() {
         assert_eq!(tup2.val(i), Val(i + 42));
     }
     unsafe {
-        assert_eq!(tup2.0.add(Tup::short_size(4) as usize), tup1.0);
-        assert!(mem.pools[0].own(tup1.0));
-        assert!(mem.pools[0].own(tup2.0));
+        assert_eq!(tup2.0.add(Tup::words_from_words(4) as usize), tup1.0);
+        assert!(mem.minor_pool.own(tup1.0));
+        assert!(mem.minor_pool.own(tup2.0));
     }
 }
 
 #[test]
 fn minor_gc_00() {
     let word_size = std::mem::size_of::<usize>();
-    let mut mem = Mem::new(16, 16, 32 * word_size, 32 * word_size);
-    mem.alloc_short(4, 1).unwrap();
-    assert_eq!(mem.pools[0].left, 27);
-    mem.alloc_short(4, 1).unwrap();
-    assert_eq!(mem.pools[0].left, 22);
-    mem.alloc_short(4, 1).unwrap();
-    assert_eq!(mem.pools[0].left, 17);
-    mem.alloc_short(10, 1).unwrap();
-    assert_eq!(mem.pools[0].left, 6);
+    let mut mem = Mem::new(16, 16, 32 * word_size);
+    mem.alloc_short(4, 1);
+    assert_eq!(mem.minor_pool.left, 27);
+    mem.alloc_short(4, 1);
+    assert_eq!(mem.minor_pool.left, 22);
+    mem.alloc_short(4, 1);
+    assert_eq!(mem.minor_pool.left, 17);
+    mem.alloc_short(10, 1);
+    assert_eq!(mem.minor_pool.left, 6);
     // GC invoked
-    mem.alloc_short(10, 1).unwrap();
-    assert_eq!(mem.pools[0].left, 21);
+    mem.alloc_short(10, 1);
+    assert_eq!(mem.minor_pool.left, 21);
 }
 
 #[test]
 fn minor_gc_01() {
     let word_size = std::mem::size_of::<usize>();
-    let mut mem = Mem::new(16, 16, 32 * word_size, 128 * word_size);
+    let mut mem = Mem::new(16, 16, 32 * word_size);
     // Create a garbage
-    mem.alloc_short(4, 41).unwrap();
-    assert_eq!(mem.pools[0].left, 27);
+    mem.alloc_short(4, 41);
+    assert_eq!(mem.minor_pool.left, 27);
     // Create a non-garbate
-    let v = mem.alloc_short(4, 42).unwrap().to_val();
-    assert_eq!(mem.pools[0].left, 22);
-    mem.stack.push(v);
+    let v = mem.alloc_short(4, 42).to_val();
+    assert_eq!(mem.minor_pool.left, 22);
+    mem.stack.push(v.0);
     // Create one more garbage
-    mem.alloc_short(4, 43).unwrap();
-    assert_eq!(mem.pools[0].left, 17);
+    mem.alloc_short(4, 43);
+    assert_eq!(mem.minor_pool.left, 17);
     // Create one more non-garbage
-    let w = mem.alloc_short(4, 44).unwrap().to_val();
-    assert_eq!(mem.pools[0].left, 12);
-    mem.stack.push(w);
+    let w = mem.alloc_short(4, 44).to_val();
+    assert_eq!(mem.minor_pool.left, 12);
+    mem.stack.push(w.0);
     // GC invoked
-    mem.collect_minor().unwrap();
-    assert_eq!(mem.pools[0].left, 32);
-    assert_eq!(mem.pools[1].left, 118);
+    mem.collect_minor();
+    assert_eq!(mem.minor_pool.left, 32);
+    //assert_eq!(mem.pools[1].left, 118);
     for i in 0..2 {
-        let tup = Tup::from_val(mem.stack.get(i));
-        assert!(mem.pools[1].own(mem.stack.get(i).to_gc_ptr()));
+        let tup = Tup::from_val(Val(mem.stack[i]));
+        assert!(!mem.minor_pool.own(Val(mem.stack[i]).to_gc_ptr()));
         assert!(tup.tag() == 42 + 2 * i);
         assert!(tup.header().is_white());
         assert!(!tup.is_long());
         assert!(tup.len() == 4);
     }
     unsafe {
-        assert!(
-            mem.stack.get(0).to_gc_ptr::<usize>().add(5)
-                == mem.stack.get(1).to_gc_ptr()
-                || mem.stack.get(1).to_gc_ptr::<usize>().add(5)
-                    == mem.stack.get(0).to_gc_ptr()
-        );
+        let p0 = Val(mem.stack[0]).to_gc_ptr::<usize>();
+        let p1 = Val(mem.stack[1]).to_gc_ptr::<usize>();
+        assert!(p0.add(5) == p1 || p1.add(5) == p0);
     }
 }
 
 #[test]
 fn minor_gc_02() {
     let word_size = std::mem::size_of::<usize>();
-    let mut mem = Mem::new(16, 16, 32 * word_size, 128 * word_size);
-    // Create lots of garbage
+    let mut mem = Mem::new(16, 16, 32 * word_size);
     for _i in 0..100 {
-        mem.alloc_short(4, 0).unwrap();
-        mem.alloc_long(48).unwrap();
+        mem.alloc_short(4, 0);
+        mem.alloc_long(48);
     }
     // Create non-garbage
-    let v = mem.alloc_short(4, 42).unwrap().to_val();
-    mem.stack.push(v);
+    let v = mem.alloc_short(4, 42).to_val();
+    mem.stack.push(v.0);
     for i in 0..4 {
         Tup::from_val(v).set_val(i, Val(i + 123));
     }
     // Create lots of garbage
     for _i in 0..10 {
-        mem.alloc_short(4, 0).unwrap();
-        mem.alloc_long(41).unwrap();
+        mem.alloc_short(4, 0);
+        mem.alloc_long(41);
     }
     // Create non-garbatge
-    let w = mem.alloc_long(56).unwrap().to_val();
-    mem.stack.push(w);
+    let w = mem.alloc_long(56).to_val();
+    mem.stack.push(w.0);
     let msg = "Hello, World!";
     unsafe {
         for i in 0..msg.len() {
@@ -132,61 +126,52 @@ fn minor_gc_02() {
     }
     // Create lots of garbage
     for _i in 0..10 {
-        mem.alloc_short(4, 0).unwrap();
-        mem.alloc_long(46).unwrap();
+        mem.alloc_short(4, 0);
+        mem.alloc_long(46);
     }
-    assert_eq!(
-        (mem.pools[1].vals - mem.pools[1].left) as usize,
-        (56 / word_size) + 1 + 5
-    );
     // Check first value
-    let tup_v = Tup::from_val(mem.stack.get(0));
-    assert!(mem.pools[1].own(tup_v.0));
+    let tup_v = Tup::from_val(Val(mem.stack[0]));
     assert!(!tup_v.is_long());
     assert!(tup_v.tag() == 42);
     for i in 0..4 {
         assert!(tup_v.val(i) == Val(i + 123));
     }
     // Check second value
-    let tup_w = Tup::from_val(mem.stack.get(1));
-    assert!(mem.pools[1].own(tup_w.0));
+    let tup_w = Tup::from_val(Val(mem.stack[0]));
     assert!(tup_w.is_long());
     for i in 0..msg.len() {
-        assert_eq!(tup_w.byte_at(i as u64), msg.as_bytes()[i]);
+        assert_eq!(tup_w.byte_at(i), msg.as_bytes()[i]);
     }
 }
 
 #[test]
 fn major_gc_00() {
     let word_size = std::mem::size_of::<usize>();
-    let mut mem = Mem::new(16, 16, 32 * word_size, 128 * word_size);
+    let mut mem = Mem::new(16, 16, 32 * word_size);
     // Create a garbage
-    mem.alloc_short(4, 41).unwrap();
-    assert_eq!(mem.pools[0].left, 27);
+    mem.alloc_short(4, 41);
+    assert_eq!(mem.minor_pool.left, 27);
     // Create a non-garbate
-    let v = mem.alloc_short(4, 42).unwrap().to_val();
-    assert_eq!(mem.pools[0].left, 22);
-    mem.stack.push(v);
+    let v = mem.alloc_short(4, 42).to_val();
+    assert_eq!(mem.minor_pool.left, 22);
+    mem.stack.push(v.0);
     // Create one more garbage
-    mem.alloc_short(4, 43).unwrap();
-    assert_eq!(mem.pools[0].left, 17);
+    mem.alloc_short(4, 43);
+    assert_eq!(mem.minor_pool.left, 17);
     // Create one more non-garbage
-    let w = mem.alloc_short(4, 44).unwrap().to_val();
-    assert_eq!(mem.pools[0].left, 12);
-    mem.global.push(w);
+    let w = mem.alloc_short(4, 44).to_val();
+    assert_eq!(mem.minor_pool.left, 12);
+    mem.global.push(w.0);
     // GC invoked
-    mem.collect_minor().unwrap();
-    assert_eq!(mem.pools[0].left, 32);
-    assert_eq!(mem.pools[1].left, mem.pools[1].vals - 10);
+    mem.collect_minor();
+    assert_eq!(mem.minor_pool.left, 32);
     // Clean-up all stacks
     mem.stack.pop();
     mem.global.pop();
     // Run minor
-    mem.collect_minor().unwrap();
-    assert_eq!(mem.pools[0].left, 32);
-    assert_eq!(mem.pools[1].left, mem.pools[1].vals - 10);
+    mem.collect_minor();
+    assert_eq!(mem.minor_pool.left, 32);
     // Run Major
-    mem.collect_major(0).unwrap();
-    assert_eq!(mem.pools[0].left, 32);
-    assert_eq!(mem.pools[1].left, mem.pools[1].vals);
+    mem.collect_major();
+    assert_eq!(mem.minor_pool.left, 32);
 }
--- tests/pool_test.rs
+++ tests/pool_test.rs
@@ -12,7 +12,7 @@ fn allocate_values() {
     let old_left = pool.left;
     // Allocate
     let tup = pool.alloc(16);
-    assert!(tup.is_ok());
+    assert!(tup.is_some());
     assert_eq!(old_left, pool.left + 16);
     // Check pointer
     let tup = tup.unwrap();
@@ -27,10 +27,10 @@ fn allocate_two_values() {
     let mut pool = Pool::new(1024);
     // Allocate one
     let tup1 = pool.alloc(16);
-    assert!(tup1.is_ok());
+    assert!(tup1.is_some());
     // Allocate another one
     let tup2 = pool.alloc(16);
-    assert!(tup2.is_ok());
+    assert!(tup2.is_some());
     // Check pointer
     unsafe {
         tup2.unwrap().add(16).write(42);
@@ -42,9 +42,9 @@ fn allocate_two_values() {
 fn allocation_failed() {
     const WORD_SIZE: usize = std::mem::size_of::<usize>();
     let mut pool = Pool::new(WORD_SIZE * 100);
-    assert!(pool.alloc(30).is_ok());
-    assert!(pool.alloc(100).is_err());
-    assert!(pool.alloc(71).is_err());
+    assert!(pool.alloc(30).is_some());
+    assert!(pool.alloc(100).is_none());
+    assert!(pool.alloc(71).is_none());
 }
 
 #[test]
@@ -52,14 +52,14 @@ fn allocate_short() {
     const WORD_SIZE: usize = std::mem::size_of::<usize>();
     let mut pool = Pool::new(1024);
     let tup = pool.alloc_short(4, 12);
-    assert!(tup.is_ok());
+    assert!(tup.is_some());
     let tup = tup.unwrap();
     let hd = tup.header();
     assert!(!hd.is_long());
     assert!(hd.is_white());
     assert_eq!(hd.tag(), 12);
-    assert_eq!(hd.size(), 4);
-    assert_eq!(pool.left, (1024 / WORD_SIZE) as val::usize - 5);
+    assert_eq!(hd.len(), 4);
+    assert_eq!(pool.left, (1024 / WORD_SIZE) as usize - 5);
     unsafe {
         assert_eq!(tup.0, pool.ptr.add(pool.left as usize));
         assert_eq!(tup.0.read(), hd.0);
